5a3e4f43c3504084e3b6ae1e164412fe0a39b536
Yordan Lazarov <yordanlazarov@Dakata-765.local>
2026-04-04 23:50:15 +0300

    Auto disable on invalid

--- src/component/Button.tsx
+++ src/component/Button.tsx
@@ -1,23 +1,58 @@
-import React, {ForwardedRef, forwardRef, PropsWithChildren} from "react";
+import React, {PropsWithChildren, RefObject, useEffect, useRef} from "react";
 import BaseButtonContent, {ButtonContentProps} from "./BaseButtonContent.tsx";
 import {UsePreloader, UsePreloaderProvider} from "@src/component/Preloader.tsx";
 
-export default forwardRef((
+export default (
     {
         children,
         type,
         form,
         preload,
+        autoDisableOnInvalid,
+        ref,
         ...props
-    }: {loader?: string} & ButtonContentProps & PropsWithChildren & React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>, ref: ForwardedRef<HTMLButtonElement>) => {
+    }: {
+        loader?: string,
+        autoDisableOnInvalid?: boolean,
+        ref?: RefObject<HTMLButtonElement>
+    } & ButtonContentProps & PropsWithChildren & React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>) => {
 
     const {isLoading} = UsePreloaderProvider() || {};
     const loader = UsePreloader() || form || 'form';
+    const buttonRef = ref || useRef<HTMLButtonElement>(null);
+
     preload = preload || (type === 'submit' && isLoading?.(loader));
 
+    useEffect(() => {
+        if(!autoDisableOnInvalid) {
+            return;
+        }
+
+        const formEl = buttonRef.current?.closest('form');
+        const fieldsetEl =  buttonRef.current?.closest('fieldset, form');
+
+        const onChange = () => {
+            if(!buttonRef.current) {
+                return;
+            }
+
+            buttonRef.current.disabled = !formEl?.checkValidity();
+        }
+
+        fieldsetEl?.addEventListener('input', onChange)
+
+        if(buttonRef.current) {
+            buttonRef.current.disabled = true;
+        }
+
+        return () => {
+            fieldsetEl?.removeEventListener('input', onChange)
+        }
+    }, []);
+
     return (
-        <button {...{disabled: preload}} form={form} type={"submit"} {...props} ref={ref}>
+        <button {...{disabled: preload}} form={form} type={"submit"} {...props} ref={buttonRef}>
             <BaseButtonContent preload={preload} {...props}>{children}</BaseButtonContent>
         </button>
     );
-})
+}
