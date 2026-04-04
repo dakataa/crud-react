7b0052939f4fd65bd7e543f3b6a896817476ac58
Yordan Lazarov <yordanlazarov@Dakata-765.local>
2026-04-05 00:59:51 +0300

    fix button auto disable on validation

--- src/component/Button.tsx
+++ src/component/Button.tsx
@@ -1,4 +1,4 @@
-import React, {PropsWithChildren, RefObject, useEffect, useRef} from "react";
+import React, {PropsWithChildren, RefObject, useEffect, useRef, useState} from "react";
 import BaseButtonContent, {ButtonContentProps} from "./BaseButtonContent.tsx";
 import {UsePreloader, UsePreloaderProvider} from "@src/component/Preloader.tsx";
 
@@ -20,38 +20,52 @@ export default (
     const {isLoading} = UsePreloaderProvider() || {};
     const loader = UsePreloader() || form || 'form';
     const buttonRef = ref || useRef<HTMLButtonElement>(null);
+    const [disabled, setDisabled] = useState(autoDisableOnInvalid || false);
 
     preload = preload || (type === 'submit' && isLoading?.(loader));
 
     useEffect(() => {
-        if(!autoDisableOnInvalid) {
+        if (!autoDisableOnInvalid) {
             return;
         }
 
         const formEl = buttonRef.current?.closest('form');
-        const fieldsetEl =  buttonRef.current?.closest('fieldset, form');
+        const fieldsetEl = buttonRef.current?.closest('fieldset, form');
 
         const onChange = () => {
-            if(!buttonRef.current) {
+            if (!buttonRef.current) {
                 return;
             }
 
-            buttonRef.current.disabled = !formEl?.checkValidity();
+            const boundingClientRect = buttonRef.current.getBoundingClientRect();
+            const isVisible = !(boundingClientRect.width === 0 && boundingClientRect.height === 0);
+            if(!isVisible) {
+                return;
+            }
+
+            setDisabled(!formEl?.checkValidity() || false);
         }
 
         fieldsetEl?.addEventListener('input', onChange)
 
-        if(buttonRef.current) {
-            buttonRef.current.disabled = true;
-        }
+        setTimeout(onChange, 50);
 
         return () => {
             fieldsetEl?.removeEventListener('input', onChange)
         }
     }, []);
 
+    useEffect(() => {
+        if (!buttonRef.current) {
+            return;
+        }
+
+        buttonRef.current.disabled = preload || disabled;
+
+    }, [preload]);
+
     return (
-        <button {...{disabled: preload}} form={form} type={"submit"} {...props} ref={buttonRef}>
+        <button form={form} type={"submit"} disabled={disabled} {...props} ref={buttonRef}>
             <BaseButtonContent preload={preload} {...props}>{children}</BaseButtonContent>
         </button>
     );
