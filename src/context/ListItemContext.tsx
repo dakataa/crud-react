a783e0435576d64de8eed20df4829490360cfe3c
Yordan Lazarov <yordanlazarov@Dakata-765.local>
2026-08-05 10:04:11 +0300

    fix

--- src/context/ListItemContext.tsx
+++ src/context/ListItemContext.tsx
@@ -1,20 +1,24 @@
 import React, {PropsWithChildren} from "react";
 
-export type ListItemContextType = {
+export type ListItemContextType<
+    TData = Record<string, any>
+> = {
     index: number;
     id?: string | number;
-    data: Record<string, unknown>;
+    data: TData;
 }
 
 const ListItemContext = React.createContext<ListItemContextType | undefined>(undefined);
 
-export function UseListItem() {
+export function UseListItem<
+    TData = Record<string, any>
+>() {
     const context = React.useContext<ListItemContextType | undefined>(ListItemContext);
     if (context === undefined) {
         throw new Error("UseListItem must be within ListItemProvider")
     }
 
-    return context;
+    return context as ListItemContextType<TData>;
 }
 
 export function ListItemProvider({children, ...props}: ListItemContextType & PropsWithChildren) {
